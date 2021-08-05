134581e5281eaf1ef9d735694fd1347a7c5f324b
ricardogk3 <rgk19@hotmail.com>
2021-08-05 16:44:36 -0300

    teste do ricardo

--- src/componentes/Produtos.jsx
+++ src/componentes/Produtos.jsx
@@ -8,6 +8,7 @@ export default function Produtos  ({navigation}){
   return(
     <View style={{flex:1,alignItems:'center' ,justifyContent:'center'}}>
       <Text>Tela - Produtos</Text>
+      <Text>O ricardo editou isso2</Text>
       <Button title=" Abrir menu lateral " onPress={() => navigation.openDrawer()}/>
     </View>
 );
