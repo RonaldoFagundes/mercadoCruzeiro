c7efd42eb8e55f2d6c76c20df6b854ad73d644dd
Jerson Barros <83137099+Dairone@users.noreply.github.com>
2021-08-05 17:21:09 -0300

    jerson

--- src/componentes/Produtos.jsx
+++ src/componentes/Produtos.jsx
@@ -9,6 +9,7 @@ export default function Produtos  ({navigation}){
     <View style={{flex:1,alignItems:'center' ,justifyContent:'center'}}>
       <Text>Tela - Produtos</Text>
       <Text>O ricardo editou isso2</Text>
+      <Text>eu tbm</Text>
       <Button title=" Abrir menu lateral " onPress={() => navigation.openDrawer()}/>
     </View>
 );
